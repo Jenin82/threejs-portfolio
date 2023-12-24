97ff6a5acd6e0eba8717d995dc392686b8e6e075
Jenin82 <jenin8282@gmail.com>
2023-12-24 22:24:21 +0530

    hero 3d component fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,30 +1,39 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
     const computer = useGLTF("./desktop_pc/scene.gltf");
+	const groupRef = useRef();
+
+    useFrame(() => {
+        if (groupRef.current) {
+            groupRef.current.rotation.y += 0.002; // Adjust rotation speed here
+        }
+    });
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
-            <spotLight
-                position={[-20, 50, 10]}
-                angle={0.12}
-                penumbra={1}
-                intensity={1}
-                castShadow
-                shadow-mapSize={1024}
-            />
-            <pointLight intensity={1} />
-            <primitive
-                object={computer.scene}
-                scale={isMobile ? 0.7 : 0.75}
-                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-                rotation={[-0.01, -0.2, -0.1]}
-            />
+            <group ref={groupRef}>
+                <hemisphereLight intensity={0.15} groundColor="black" />
+                <spotLight
+                    position={[-20, 50, 10]}
+                    angle={0.12}
+                    penumbra={1}
+                    intensity={1}
+                    castShadow
+                    shadow-mapSize={1024}
+                />
+                <pointLight intensity={1} />
+                <primitive
+                    object={computer.scene}
+                    scale={isMobile ? 0.7 : 0.75}
+                    position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+                />
+            </group>
         </mesh>
     );
 };
@@ -55,7 +64,7 @@ const ComputersCanvas = () => {
 
     return (
         <Canvas
-            frameloop="demand"
+            // frameloop="demand"
             shadows
             dpr={[1, 2]}
             camera={{ position: [20, 3, 5], fov: 25 }}
@@ -64,6 +73,7 @@ const ComputersCanvas = () => {
             <Suspense fallback={<CanvasLoader />}>
                 <OrbitControls
                     enableZoom={false}
+					enablePanning={false}
                     maxPolarAngle={Math.PI / 2}
                     minPolarAngle={Math.PI / 2}
                 />
