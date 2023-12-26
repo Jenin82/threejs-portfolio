b721f21fbce81d82537875f0098e0b97521f42e3
Jenin82 <jenin8282@gmail.com>
2023-12-26 16:06:06 +0530

    motion fix in project title

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -73,13 +73,13 @@ const ProjectCard = ({
 const Works = () => {
     return (
         <>
-            <motion.div variants={textVariant()}>
+            <div variants={textVariant()}>
                 <p className={`${styles.sectionSubText} `}>My work</p>
                 <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-            </motion.div>
+            </div>
 
             <div className="w-full flex">
-                <motion.p
+                <p
                     variants={fadeIn("", "", 0.1, 1)}
                     className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
                 >
@@ -89,7 +89,7 @@ const Works = () => {
                     demos in it. It reflects my ability to solve complex
                     problems, work with different technologies, and manage
                     projects effectively.
-                </motion.p>
+                </p>
             </div>
 
             <div className="mt-20 flex flex-wrap gap-7">
